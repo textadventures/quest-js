787f3178b678a48a36ec9938b9d120087c166088
Alex Warren <alex@axeuk.com>
2013-07-23 03:05:57 +0100

    Experimenting with an implementation of object attributes

--- Prototypes/Knockout/game.js
+++ Prototypes/Knockout/game.js
@@ -11,12 +11,34 @@ function GameObject(name) {
 	var self = this;
 	self.name = name;
 	self.attributes = ko.observableArray();
+
+	self.set = function(attribute, value) {
+		if (!self.hasOwnProperty(attribute)) {
+			self.attributes.push(attribute);
+			self[attribute] = ko.observable(value);
+			console.log("Added new attribute: " + self.name + "." + attribute + "=" + value);
+		}
+		else {
+			self[attribute](value);
+			console.log("Updated attribute: " + self.name + "." + attribute + "=" + value);
+		}
+	}
 }
 
 $(function(){
 	var game = new GameModel();
-	game.objects.push(new GameObject("book"));
+	var book = new GameObject("book");
+	game.objects.push(book);
+	book.set("look", "Initial look description");
+	book.set("look", "Updated look description");
+	book.set("take", function(){
+		console.log ("Ran initial take function");
+	});
+	book.set("take", function(){
+		console.log ("Ran updated take function");
+	});
 	ko.applyBindings(game);
 
-	$("#game-content").html("Initialised");
+	$("#game-content").html("Initialised. Book description is '" + book.look() + "'");
+	book.take()();
 });
